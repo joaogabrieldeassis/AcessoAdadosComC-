ecd128d1c75884ac04cf2cc17d5e24bd2c12a9be
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-04-22 08:36:16 -0300

    Subindo a primeira atualização com seus devidos arquivos adicionados

---
+++ baltaDataAccess/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace BaltaDataAccess // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conectando nosso banco 
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
