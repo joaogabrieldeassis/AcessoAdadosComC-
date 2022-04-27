cf9dffd92fd35545ded421fb9fb312090f4e084a
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-04-27 14:07:23 -0300

    Adicionando as carreiras

---
+++ Modulo3/Model/CareerItem.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Modulo3.Model
+{
+    public class CareerItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Course Course { get; set; }
+    }
+}
