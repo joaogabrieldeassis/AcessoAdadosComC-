140e314b2d488ff196262390b785d215a04c4b64
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-05-01 11:49:45 -0300

    Atualizando o program

--- Modulo4/blog/Program.cs
+++ Modulo4/blog/Program.cs
@@ -13,87 +13,30 @@ namespace Modulo4.Blog // Note: actual namespace depends on the project name.
         {
             var connection = new SqlConnection(connectionString);
             connection.Open();
-            //ReadUsers();
-            //ReadUser();
-            // CreateUser();
-            //UpdateUser();
-            //DeleteUser();
+            ReadUser(connection);
+            ReadRole(connection);
             connection.Close();
 
         }
         //Método para buscar no banco os user
 
         //Método para buscar no banco os users com o id
+        public static void ReadRole(SqlConnection connection)
+        {
+            var receiveRoleRepository = new RoleRepository(connection);
+            var roles = receiveRoleRepository.Get();
+            foreach (var item in roles)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
         public static void ReadUser(SqlConnection connection)
         {
-            var receiveUserRepository = new UserRepository(connection);
-            var users = receiveUserRepository.Get();
+            var receiveUsers = new UserRepository(connection);
+            var users = receiveUsers.Get();
             foreach (var item in users)
             {
-                Console.WriteLine(users);
-            }
-
-        }
-        //Inserindo informações no banco com o método Insert
-        public static void CreateUser()
-        {
-            var user = new User()
-            {
-                Bio = "Joao",
-                Email = "Joao@ifaksjaopifh",
-                Name = "João e mari",
-                PasswordHash = "akfihjaafo",
-                Slug = "aófkd",
-                Image = "https/Joaogostoso"
-
-            };
-            using (var connection = new SqlConnection(connectionString))
-            {
-                connection.Insert<User>(user);
-
-                Console.WriteLine("Usuario cadastrado");
-
-
-            }
-
-        }
-        // Fazendo o update com o método abaixo
-        public static void UpdateUser()
-        {
-            var user = new User()
-            {
-                Id = 4,
-                Bio = "Joao",
-                Email = "Joao@ifaksjaopifh",
-                Name = "João e mari casa carro",
-                PasswordHash = "akfihjaafo",
-                Slug = "aófkd",
-                Image = "https/Joaogostoso"
-
-            };
-            using (var connection = new SqlConnection(connectionString))
-            {
-                connection.Update<User>(user);
-
-                Console.WriteLine("Usuario cadastrado");
-
-
-            }
-
-        }
-        // Excluindo um usuario com 
-        public static void DeleteUser()
-        {
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                //Get busca só o usuario com o id 1
-                var user = connection.Get<User>(1);
-                connection.Delete<User>(user);
-
-                Console.WriteLine("Usuario Excluido");
-
-
+                Console.WriteLine(item.Name);
             }
 
         }
