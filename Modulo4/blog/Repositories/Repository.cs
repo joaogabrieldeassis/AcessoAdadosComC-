f5353181ba605547d5a9bd8b3e4cd36bcd647229
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-05-02 14:55:13 -0300

    Otimizando ainda mais o meu repository

--- Modulo4/blog/Repositories/Repository.cs
+++ Modulo4/blog/Repositories/Repository.cs
@@ -1,11 +1,15 @@
+using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
+using Modulo4.Blog.Models;
 
 namespace Modulo4.Blog.Repositories
 {
-    public class Repository
+    public class Repository<T> where T : class
     {
         private readonly SqlConnection _conection;
-        public UserRepository(SqlConnection connection)
+        public Repository(SqlConnection connection)
             => _conection = connection;
+        public IEnumerable<T> Get()
+            => _conection.GetAll<T>();
     }
 }
