897b8b0c6697f5dd10ef9dfe4d049af5e51100ad
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-05-03 15:06:06 -0300

    Adicionando a classe User repository

--- Modulo4/blog/Repositories/UserRepository.cs
+++ Modulo4/blog/Repositories/UserRepository.cs
@@ -1,22 +1,14 @@
-using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 using Modulo4.Blog.Models;
 
 namespace Modulo4.Blog.Repositories
 {
-    public class UserRepository
+    //Essa classe sera para ler o meus usuarios e meu Role juntos 
+    public class UserRepository : Repository<User>
     {
-        private readonly SqlConnection _conectionUser;
+        private readonly SqlConnection _conection;
         public UserRepository(SqlConnection connection)
-        {
-            _conectionUser = connection;
-        }
-        public IEnumerable<User> Get()
-        => _conectionUser.GetAll<User>();
-        public User Get(int id)
-        => _conectionUser.Get<User>(id);
-        public void Create(User user)
-        => _conectionUser.Insert<User>(user);
-
+        : base(connection)
+            => _conection = connection;
     }
 }
