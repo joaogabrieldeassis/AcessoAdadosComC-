4f4bc27810b0cf676b2f9c3d7006a3e54f22aed8
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-05-09 16:56:14 -0300

    Deletando o usuario

--- Modulo4/blog/Screens/UserScreens/DeletUserScreens.cs
+++ Modulo4/blog/Screens/UserScreens/DeletUserScreens.cs
@@ -1,13 +1,35 @@
+using Modulo4.Blog.Models;
+using Modulo4.Blog.Repositories;
+
 namespace Modulo4.Blog.Screens.UserScreens
 {
     public static class DeletUserScreens
     {
         public static void Load()
         {
-
+            Console.Clear();
+            Console.WriteLine("Digite o Id do usuario que deseja excluir");
+            var id = Console.ReadLine();
+            InsertUser(int.Parse(id));
+            Console.ReadKey();
+            Console.WriteLine("Usuario Deletado com sucesso");
+            Console.WriteLine("Precione Enter para voltar ao Menu");
+            MenuUserScreen.Load();
         }
-        public static void InsertUser()
+        public static void InsertUser(int id)
         {
+            try
+            {
+                var connectionDelet = new Repository<User>(Database.Connection);
+                connectionDelet.Delete(id);
+
+
+            }
+            catch (System.Exception)
+            {
+
+                Console.WriteLine("Usuario não existente ou já excluido! Tente novamente");
+            }
 
         }
     }
